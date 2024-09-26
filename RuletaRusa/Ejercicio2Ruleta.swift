e0d41dcc99381c509ea4f25154288747f9374a1d
Joselu Mac <escrita15@gmail.com>
2024-09-26 14:01:18 +0200

    Creando un buen repositorio...

---
+++ RuletaRusa/Ejercicio2Ruleta.swift
@@ -0,0 +1,9 @@
+//
+//  Ejercicio2Ruleta.swift
+//  RuletaRusa
+//
+//  Created by Sofía Bongartz on 26/9/24.
+//
+func EjercicioRuleta(){
+print("FUnciona")
+}
--- RuletaRusa/main.swift
+++ RuletaRusa/main.swift
@@ -8,4 +8,4 @@
 import Foundation
 
 print("Ruleta rusa")
-
+//EjercicioRuleta()
